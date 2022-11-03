4f38d4e02414b18638910d695c9ba6b6d0d8f122
Jumi Yoon <jums.code@gmail.com>
2022-11-02 22:05:53 -0700

    commit tree c3acada0b1c86ed6d7457ef288b08514ce2d4e35

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    function alerting() {
+        alert ('I have been invoked!')
+    }
+    input.addEventListener ('click', alerting)
 }
+
+
